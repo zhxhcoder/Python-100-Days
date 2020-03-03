b4d61b5f149247b37e9fcea94b767b0b6d0a842c
zhxh <zhxhcoder@gmail.com>
2020-03-03 09:41:08 +0800

    把@asyncio.coroutine替换为async；把yield from替换为await。

--- Day01-15/code/Day13/asyncio1.py
+++ Day01-15/code/Day13/asyncio1.py
@@ -8,15 +8,18 @@ Date: 2018-03-21
 
 import asyncio
 import threading
+
+
 # import time
 
+# 1.把@asyncio.coroutine替换为async；
+# 2.把yield from替换为await。
 
-@asyncio.coroutine
-def hello():
+async def hello():
     print('%s: hello, world!' % threading.current_thread())
     # 休眠不会阻塞主线程因为使用了异步I/O操作
     # 注意有yield from才会等待休眠操作执行完成
-    yield from asyncio.sleep(2)
+    await asyncio.sleep(2)
     # asyncio.sleep(1)
     # time.sleep(1)
     print('%s: goodbye, world!' % threading.current_thread())
